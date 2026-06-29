b4b6457c3646dd94afbcd4f1eeb95fbcfc92666a
Jasper de Waal / JappiePappie <s1155590@student.hsleiden.nl>
2026-06-29 11:13:29 +0200

    [FIX] bugfix in pipeline

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,36 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
-import { defineConfig } from "eslint/config";
+import vueParser from "vue-eslint-parser";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  pluginVue.configs["flat/essential"],
-  { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
-]);
+export default [
+  {
+    ignores: ["**/dist", "**/node_modules", "**/public"],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+  {
+    files: ["**/*.vue"],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tseslint.parser,
+        sourceType: "module",
+      },
+      // HIER IS DE FIX: Voeg dit blok toe
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+];
